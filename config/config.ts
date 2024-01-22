960883966384873c62fdf829113349d8a9c87bc8
deling.mdl <lingnck@163.com>
2024-01-22 22:19:09 +0800

    验证

--- config/config.ts
+++ config/config.ts
@@ -46,10 +46,8 @@ export default defineConfig({
   title: false,
   ignoreMomentLocale: true,
   proxy: proxy[REACT_APP_ENV || 'dev'],
-  base: '/mlnbook',
-  publicPath: '/mlnbook/',
   manifest: {
-    basePath: '/mlnbook',
+    basePath: '/',
   },
   // Fast Refresh 热更新
   fastRefresh: {},
